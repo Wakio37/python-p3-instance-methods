14a4fcec456cb19397d466bfe04d82fd8e499140
Wakio37 <your_email@example.com>
2023-10-10 15:19:53 +0000

    completed assignment

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
 class Person:
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
+    
+peris = Person()
+peris.talk()
+peris.walk()
+    
+
     # Class body goes here
 
     #Instance method definition
-    pass
+    
